f6d4e084f735aacb461ddb363e809fd8cee15fe2
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-04-14 16:17:33 -0300

    add mapeamento

---
+++ mapeamento/pcihello_restored/pcihellocore/pcihellocore_bb.v
@@ -0,0 +1,28 @@
+
+module pcihellocore (
+	pcie_hard_ip_0_rx_in_rx_datain_0,
+	pcie_hard_ip_0_tx_out_tx_dataout_0,
+	pcie_hard_ip_0_powerdown_pll_powerdown,
+	pcie_hard_ip_0_powerdown_gxb_powerdown,
+	pcie_hard_ip_0_refclk_export,
+	pcie_hard_ip_0_pcie_rstn_export,
+	hexport_external_connection_export,
+	inport_external_connection_export,
+	hex_display_external_connection_export,
+	led_red_external_connection_export,
+	led_green_external_connection_export,
+	switch_external_connection_export);	
+
+	input		pcie_hard_ip_0_rx_in_rx_datain_0;
+	output		pcie_hard_ip_0_tx_out_tx_dataout_0;
+	input		pcie_hard_ip_0_powerdown_pll_powerdown;
+	input		pcie_hard_ip_0_powerdown_gxb_powerdown;
+	input		pcie_hard_ip_0_refclk_export;
+	input		pcie_hard_ip_0_pcie_rstn_export;
+	output	[31:0]	hexport_external_connection_export;
+	input	[15:0]	inport_external_connection_export;
+	output	[31:0]	hex_display_external_connection_export;
+	output	[31:0]	led_red_external_connection_export;
+	output	[31:0]	led_green_external_connection_export;
+	input	[31:0]	switch_external_connection_export;
+endmodule
