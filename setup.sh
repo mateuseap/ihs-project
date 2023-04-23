0511268f59572ad3f4a7361ad312679138e1d370
Mateus Elias <meap@cin.ufpe.br>
2023-04-23 02:04:08 -0300

    feat(setup.sh): removed unused steps and started running python files

--- setup.sh
+++ setup.sh
@@ -3,14 +3,13 @@
 GREEN='\033[0;32m'
 CLEAR='\033[0m'
 
-echo -e "$GREEN BUILDING APP $CLEAR"
-make
-
 echo -e "$GREEN BUILDING DRIVER $CLEAR"
 cd driver/pci
 make
+
 echo -e "$GREEN REMOVING OLD DRIVERS $CLEAR"
 sudo rmmod de2i_150
+
 echo -e "$GREEN INSERTING DRIVER TO KERNEL $CLEAR"
 sudo insmod de2i-150.ko
 cd ../..
@@ -18,8 +17,11 @@ cd ../..
 echo -e "$GREEN CHANGING DEVICE NODE PERMISSIONS $CLEAR"
 sudo chmod 666 /dev/mydev
 
-echo -e "$GREEN DONE! $CLEAR"
-echo ""
-echo -e "$GREEN IN ORDER TO RUN THE APPLICATION RUN: $CLEAR"
-echo -e "$GREEN\t ./app/build/release/app $CLEAR"
+echo -e "$GREEN RUNNING START FILE $CLEAR"
+cd app
+python start.py
 
+echo -e "$GREEN RUNNING THE GAME $CLEAR"
+python game.py
+
+echo -e "$GREEN DONE! $CLEAR"
