51324c1e1f5cfe08d3c0534549e897d7823aa4b6
Mateus Elias <meap@cin.ufpe.br>
2023-04-24 17:16:22 -0300

    feat(app/start.py): implementation modified

--- app/start.py
+++ app/start.py
@@ -1,4 +1,5 @@
 from utils import *
+from time import sleep
 
 fd = os.open(PATH, os.O_RDWR)
 print('Arquivo aberto com sucesso!')
@@ -6,15 +7,14 @@ print('Arquivo aberto com sucesso!')
 def reset():
     global fd
 
-    seven_segment(fd=fd, num=0, display=WR_L_DISPLAY, show_output_msg=True)
-    seven_segment(fd=fd, num=0, display=WR_R_DISPLAY, show_output_msg=True)
-    red_leds(fd=fd, on=False, sequence=False, show_output_msg=True)
-    green_leds(fd=fd, on=False, sequence=False, show_output_msg=True)
+    red_leds(fd=fd, on=False, inverse=False, sequence=False, show_output_msg=True)
+    green_leds(fd=fd, on=False, inverse=False, sequence=False, show_output_msg=True)
     read_switches(fd=fd, show_output_msg=True)
     read_button(fd=fd, show_output_msg=True)
+    read_button(fd=fd, show_output_msg=True)    
 
     os.close(fd)
-    print('Arquivo fechado com sucesso!')
+    print('\nArquivo fechado com sucesso!\n')
 
 def start():
     global fd
@@ -25,24 +25,24 @@ def start():
         button = read_button(fd=fd, show_output_msg=False)
         if BUTTONS_OPTIONS[button] == "START":
             break
-    
-    # Showing numbers on seven segment displays
-    seven_segment(fd=fd, num=1, display=WR_L_DISPLAY, show_output_msg=True)
-    seven_segment(fd=fd, num=3, display=WR_R_DISPLAY, show_output_msg=True)
 
     # Turning on all leds
-    red_leds(fd=fd, on=True, sequence=False, show_output_msg=True)
-    green_leds(fd=fd, on=True, sequence=False, show_output_msg=True)
+    red_leds(fd=fd, on=True, inverse=False , sequence=False, show_output_msg=True)
+    green_leds(fd=fd, on=True, inverse=False , sequence=False, show_output_msg=True)
     
     # Turning off all leds
-    red_leds(fd=fd, on=False, sequence=False, show_output_msg=True)
-    green_leds(fd=fd, on=False, sequence=False, show_output_msg=True)
+    red_leds(fd=fd, on=False, inverse=False, sequence=False, show_output_msg=True)
+    green_leds(fd=fd, on=False, inverse=False, sequence=False, show_output_msg=True)
 
     # Turning on one led at a time in sequence
-    red_leds(fd=fd, sequence=True, show_output_msg=True)
-    green_leds(fd=fd, sequence=True, show_output_msg=True)
+    red_leds(fd=fd, on=False, inverse=False, sequence=True, show_output_msg=True)    
+    green_leds(fd=fd, on=False, inverse=False, sequence=True, show_output_msg=True)
 
-    print('\nDesligue todos os switches para o jogo começar!\n')
+    # Turning on one led at a time in a inverse sequence
+    green_leds(fd=fd, on=False, inverse=True, sequence=True, show_output_msg=True)  
+    red_leds(fd=fd, on=False, inverse=True, sequence=True, show_output_msg=True)    
+
+    print('\nLigue todos os switches para o jogo começar!\n')
 
     while True:
         switches = read_switches(fd=fd, show_output_msg=False)
